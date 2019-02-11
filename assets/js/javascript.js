15b25cb6fc01b1a721e06ed96656d9dada0424ad
Dom <dominicsmith1021@gmail.com>
2019-02-11 16:30:39 -0500

    Temporary scenery

---
+++ assets/js/javascript.js
@@ -0,0 +1,34 @@
+$("header .background").animate({"text-indent":"1.75"},{
+    duration:20000,
+    step: function(now,fx) {
+        $(this).css("transform","scale(" + now + ")");
+    },
+    complete:function() {
+        $(this).css("text-indent","0");
+        $(this).animate({"text-indent":"10"},{
+            duration:15000,
+            step: function(now,fx) {
+                $(this).css("transform","scale(1.75) translate(" + now + "vw)");
+            },
+            complete:function() {
+                $(this).animate({"text-indent":"-10"},{
+                    duration:15000,
+                    step: function(now,fx) {
+                        $(this).css("transform","scale(1.75) translate(" + now + "vw)");
+                    },
+                    complete:function() {
+                        $(this).animate({"text-indent":"0"},{
+                            duration:15000,
+                            step: function(now,fx) {
+                                $(this).css("transform","scale(" + Math.max(Math.min(1.75,1.75-((1/(Math.abs(now)))*0.75)),1) + ") translate(" + now + "vw)");
+                            },
+                            complete:function() {
+                                
+                            }
+                        }); 
+                    }
+                });    
+            }
+        });
+    }
+});
