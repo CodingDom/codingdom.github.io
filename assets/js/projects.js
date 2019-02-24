c8f73bcb402a0a4ccbbb71178317b971cc0d984c
Dom <dominicsmith1021@gmail.com>
2019-02-24 14:13:15 -0500

    Added extra projects

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,43 +1,52 @@
 const projectList = [
     {
-        name : "Troll RPG",
+        name : "Treasure Quest",
         desc : "",
-        score : 9,
-        thumbnail : "https://camo.githubusercontent.com/62f00063d994084f9c873c328e4fe6349ac60470/68747470733a2f2f692e6779617a6f2e636f6d2f63363136363266343161303639363239366434363338616666633663303238652e6a7067",
+        score : 6,
+        thumbnail : "https://i.imgur.com/fFuAPHU.png",
         preview : [],
-        tags : ["HTML","CSS","JavaScript", "Game Development"],
-        url : "https://codingdom.github.io/Troll-RPG",
-        repo : "https://github.com/CodingDom/Troll-RPG"
+        tags : ["HTML","CSS", "Bootstrap","JavaScript", "JQuery", "Firebase", "Game Development"],
+        url : "https://codingdom.github.io/Treasure-Quest",
+        repo : "https://github.com/CodingDom/Treasure-Quest"
+    },
+    {
+        name : "Synergy Staffing Solutions",
+        desc : "",
+        score : 6,
+        thumbnail : "https://i.imgur.com/4xmSjGK.png",
+        preview : [],
+        tags : ["HTML","CSS", "Bootstrap","JavaScript", "PHP"],
+        url : "http://synstaffing.co/"
     },
     {
         name : "Troll RPG",
         desc : "",
         score : 5,
-        thumbnail : "https://camo.githubusercontent.com/62f00063d994084f9c873c328e4fe6349ac60470/68747470733a2f2f692e6779617a6f2e636f6d2f63363136363266343161303639363239366434363338616666633663303238652e6a7067",
+        thumbnail : "https://i.imgur.com/kTWLNKL.png",
         preview : [],
         tags : ["HTML","CSS","JavaScript", "Game Development"],
         url : "https://codingdom.github.io/Troll-RPG",
         repo : "https://github.com/CodingDom/Troll-RPG"
     },
     {
-        name : "Troll RPG",
+        name : "Shell Game",
         desc : "",
-        score : 7,
-        thumbnail : "https://camo.githubusercontent.com/62f00063d994084f9c873c328e4fe6349ac60470/68747470733a2f2f692e6779617a6f2e636f6d2f63363136363266343161303639363239366434363338616666633663303238652e6a7067",
+        score : 4,
+        thumbnail : "https://i.imgur.com/qKPbsH6.png",
         preview : [],
         tags : ["HTML","CSS","JavaScript", "Game Development"],
-        url : "https://codingdom.github.io/Troll-RPG",
-        repo : "https://github.com/CodingDom/Troll-RPG"
+        url : "https://codingdom.github.io/Shell-Game",
+        repo : "https://github.com/CodingDom/Shell-Game"
     },
     {
-        name : "Troll RPG",
+        name : "Horror Trivia",
         desc : "",
-        score : 9,
-        thumbnail : "https://camo.githubusercontent.com/62f00063d994084f9c873c328e4fe6349ac60470/68747470733a2f2f692e6779617a6f2e636f6d2f63363136363266343161303639363239366434363338616666633663303238652e6a7067",
+        score : 3,
+        thumbnail : "https://i.imgur.com/7nyCBEI.png",
         preview : [],
         tags : ["HTML","CSS","JavaScript", "Game Development"],
-        url : "https://codingdom.github.io/Troll-RPG",
-        repo : "https://github.com/CodingDom/Troll-RPG"
+        url : "https://codingdom.github.io/TriviaGame",
+        repo : "https://github.com/CodingDom/TriviaGame"
     }
 ]
 
